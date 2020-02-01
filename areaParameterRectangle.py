a0e2b7b590076b683e25033ca14f09ace6ece4ee
johnsila <60229418+johnsila@users.noreply.github.com>
2020-02-01 13:31:51 +0300

    Add files via upload

---
+++ areaParameterRectangle.py
@@ -0,0 +1,8 @@
+""""area and parameter of rectangle progrm"""
+print("enter the length and width of the rectangle")
+l=int(input("lenght="))
+w=int(input("width="))
+area= l*w
+parameter=l*2+w*2
+print("the area =",area)
+print("the parameter=",parameter)
